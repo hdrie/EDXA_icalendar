991d4473db5dfc5fe8c0b5caef9a87b204f27e03
hdrie <h.driehaus@gmail.com>
2023-02-28 21:54:54 +0100

    DESCRIPTION Zeile enthält alle Dienste

--- einlesen.py
+++ einlesen.py
@@ -5,18 +5,18 @@ import pandas as pd
 # 2. zweite "Person" Spalte in "Person1" umbenennen (lässt sich besser referenzieren)
 
 # Einlesen der .CSV Datei
-dienstplan = pd.read_csv("20220306 Dienstplan-2022.csv", sep=";")
+dienstplan = pd.read_csv("20230227 Dienstplan 2023 V1.0.csv", sep=";")
 
 
 # Dictionary, um Monatsnamen in Monatsnummern umzuwandeln
 monate = {"Januar": 1, "Februar": 2, "Maerz": 3, "April": 4, "Mai": 5, "Juni": 6, \
     "Juli": 7, "August": 8, "September": 9, "Oktober": 10, "November": 11, "Dezember": 12}
-dienstplan["Monat"] = dienstplan["Monat"].replace(monate)
+dienstplan["Monat"].replace(monate, inplace=True)
 
 # Sinnvollere Dienstbezeichnungen für die Kalendereinträge
 dienstbezeichnungen = {"Fluglehrer": "Fluglehrerdienst", "Flugleiter": "Flugleiterdienst", \
-    "Windenfahrer": "Windendienst", "Windenausbilder": "Windendienst", "Kantine": "Kantinendienst"}
-dienstplan["Dienst"] = dienstplan["Dienst"].replace(dienstbezeichnungen)
+    "Windenfahrer": "Windendienst", "Windenausbilder": "Windendienst(Ausbilder)", "Kantine": "Kantinendienst"}
+dienstplan["Dienst"].replace(dienstbezeichnungen, inplace=True)
 
 # Zusammenführen der Spalten für Tag,Monat,Jahr in neuer Spalte "Date"
 dienstplan["Date"] = pd.to_datetime(dienstplan["Jahr"].astype(str) + "/" + dienstplan["Monat"].astype(str) + "/" + dienstplan["Tag"].astype(str))
@@ -25,8 +25,9 @@ dienstplan["Date"] = pd.to_datetime(dienstplan["Jahr"].astype(str) + "/" + diens
 names = pd.concat([dienstplan["Person"], dienstplan["Person1"]],ignore_index=True).drop_duplicates().dropna().reset_index(drop=True)
 
 # Zusammenführen der beiden Namensspalten. Es wird später nur das Vorkommen des jeweiligen Mitglieds innerhalb der Spalte abgefragt
-dienstplan['Person1'].fillna('keiner', inplace=True)
-dienstplan['Personen'] = dienstplan['Person'] + " ; " + dienstplan['Person1']
+dienstplan['Person1'].fillna('', inplace=True)
+dienstplan['Personen'] = dienstplan['Person'] + ' / ' + dienstplan['Person1']
+dienstplan['Personen'] = dienstplan['Personen'].str.strip(' / ')
 
 # Schreibe Dienstplan in zwischen-CSV, zu validierungszwecken
 dienstplan[["Date", "Dienst", "Personen"]].to_csv("./dp_aufbereitet.csv")
--- kalender_gen.py
+++ kalender_gen.py
@@ -6,10 +6,11 @@ import pandas as pd
 
 # Definieren einer Dienst-Klasse mit den wichtigsten Attributen.
 # Der Name der Person taucht nicht auf, da die Dienste später je Mitglied in einer eigenen Liste gesammelt werden
-class Dienst:
-    def __init__(self, typ, datum):
-        self.type = typ # Art des Dienstes: Fluglehrer, Flugleiter, Windenfahrer, Kantine
+class Diensttag:
+    def __init__(self, datum, descr, typ):
         self.date = datum
+        self.descr = descr
+        self.type = typ # Art des Dienstes: Fluglehrer, Flugleiter, Windenfahrer, Kantine
         if self.date.weekday() == 5: # Abfrage, ob der Tag ein Samstag ist; Definition der Dienstzeiten (Lokalzeit)
             self.tstart = dt.datetime.combine(self.date, dt.time(13,0,0,0))
             self.tend = dt.datetime.combine(self.date, dt.time(18,0,0,0))
@@ -17,14 +18,15 @@ class Dienst:
             self.tstart = dt.datetime.combine(self.date, dt.time(10,0,0,0))
             self.tend = dt.datetime.combine(self.date, dt.time(18,0,0,0))
 
+
 # Funktion zum Konvertieren der Lokalzeit in UTC unter Berücksichtigung der Zeitumstellung
 def local_to_utc(_dt, _tz='Europe/Berlin'):
     timezone=pytz.timezone(_tz)
     utc=pytz.utc
     return timezone.localize(_dt).astimezone(utc)
 
-# Funktion zum generieren eines icalendar-Kalenders, nimmt eine Liste von Dienst-Objekten an
-def gen_calendar(dienste):
+# Funktion zum generieren eines icalendar-Kalenders, nimmt eine Liste von Diensttages-Objekten
+def gen_calendar(diensttage):
     cal = ic.Calendar()
     cal.add('prodid', '-//Dienstplan_Achmer//von_hauken.com//')
     cal.add('version', '2.0')
@@ -32,16 +34,18 @@ def gen_calendar(dienste):
     cal.add('method', 'PUBLISH')
 
     events = {} # Generieren der einzelnen Termine innerhalb des Kalenders. Nutzen eines Dicts, um fortlaufend Events zu benennen und zu hinterlegen
-    for i,n in enumerate(dienste):
+    for i,n in enumerate(diensttage):
         events[f"event{i}"] = ic.Event()
-        events[f"event{i}"].add("uid", f"Achmer-Dienst-{n.date.isoformat()}")
+        events[f"event{i}"].add("uid", f"{n.type}-{n.date.isoformat()}")
         events[f"event{i}"].add("summary", f'{n.type} Achmer') # Name, der später im Kalender angezeigt wird
+        events[f"event{i}"].add("description", n.descr) # Ausführliche Beschreibung mit allen Diensthabenden des Tages
         events[f"event{i}"].add("dtstart", local_to_utc(n.tstart)) # Startzeit, als datetime-objekt
         events[f"event{i}"].add("dtend", local_to_utc(n.tend)) # Endzeit
         events[f"event{i}"].add("dtstamp", dt.datetime.now(dt.timezone.utc)) # Zeitstempel der Erstellung
         events[f"event{i}"].add("status", "CONFIRMED")
         cal.add_component(events[f"event{i}"])
     return cal # Rückgabe des Kalenders
+
 
 # Funktion zum schreiben des übergebenen Kalenders in eine .ics Datei. Der Name der betreffenden Person wird zum Dateinamen
 def write_calendar(name, cal):
@@ -53,13 +57,28 @@ names = pd.read_csv("./namensliste.csv")
 names = names['0'].tolist() # Vereinfachung der Namensliste als einfache liste
 dienstplan = pd.read_csv("./dp_aufbereitet.csv")
 
-# Je Mitglied in der Namensliste Erstellung einer Kalenderdatei
-for mitglied in names:
+# Erstellen einer separaten Datenbank, die den Dienstplan nach FLugbetriebstagen abbildet
+dienstplan_days = pd.read_csv("./dp_aufbereitet.csv")
+dienstplan_days['Personen'].replace("-", " ", regex=True, inplace=True)
+dienstplan_days = dienstplan_days.pivot(index='Date', columns='Dienst', values='Personen').reset_index()
+
+# Erstellen eines Kalenders mit allen Betriebstagen und einen für jedes Mitglied 
+betriebstage = [] # Liste von Flugbetriebstags-Objekten
+descr_list = {} # Dict, um den Beschreibungsstring (mit allen Diensten) nachschlagbar zwischenzuspeichern
+
+for index, row in dienstplan_days.iterrows(): # Sukzessive Erstellung von Flugbetriebstags-Objekten und Sammlung in einer Liste
+    descr = f"Flugleiter: {row['Flugleiterdienst']}\nFluglehrer: {row['Fluglehrerdienst']}\nWindenfahrer: {row['Windendienst']}\nWindenausbilder: {row['Windendienst(Ausbilder)']}\nKantine: {row['Kantinendienst']}"
+    descr_list[row['Date']] = f"Flugleiter: {row['Flugleiterdienst']}\nFluglehrer: {row['Fluglehrerdienst']}\nWindenfahrer: {row['Windendienst']}\nWindenausbilder: {row['Windendienst(Ausbilder)']}\nKantine: {row['Kantinendienst']}"
+    betriebstage.append(Diensttag(dt.date.fromisoformat(row['Date']), descr, 'Flugbetrieb'))
+
+for mitglied in names: # Je Mitglied ein Kalender: Sammeln der Relevanten Diensttage
     diensttage = dienstplan[dienstplan['Personen'].str.contains(mitglied)] # Filterung nach Zeilen, in dem das Mitglied vorkommt, also einen Dienst hat
-    dienste = []
+    dienste = [] # Liste von Dienst-Objekten
 
-    for index, row in diensttage.iterrows(): # Sukzessive Erstellung von Dienst-Objekten und Sammlung in einer Liste 
-        dienste.append(Dienst(row['Dienst'], dt.date.fromisoformat(row['Date'])))
+    for index, row in diensttage.iterrows(): # Sukzessive Erstellung von Dienstttages-Objekten und Sammlung in einer Liste
+        dienste.append(Diensttag(dt.date.fromisoformat(row['Date']), descr_list[row['Date']], row['Dienst'])) # Nachschlagen im Dict, um die restlichen Diensthabenden abzufragen
 
-    cal = gen_calendar(dienste) # Generieren des Kalenders
+    cal = gen_calendar(dienste) # Generieren des Kalenders (je Mitglied)
     write_calendar(mitglied, cal) # Schreiben des Kalenders
+cal = gen_calendar(betriebstage) # Generieren des Kalenders (Alle Flugbetriebstage)
+write_calendar('00_Dienstplan_Gesamt', cal) # Schreiben des Kalenders
